c7f130cbb27ed49abed3dc69d45b0903d815744b
bsyseo <bsyseo@naver.com>
2022-03-22 14:15:37 +0900

    added Menu.java

---
+++ src/Menu.java
@@ -0,0 +1,13 @@
+
+public class Menu {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Pets");
+		System.out.println("2. Delete Pets");
+		System.out.println("3. Edit Pets");
+		System.out.println("4. View Pets");
+		System.out.println("5. Exit");
+
+	}
+
+}
