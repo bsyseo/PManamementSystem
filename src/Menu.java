91b3ec104c95f7bc5c8658a58f2225c4ac905f85
bsyseo <bsyseo@naver.com>
2022-03-28 20:50:58 +0900

    added Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -1,109 +1,73 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Menu {
-	
+
 	private static Scanner input;
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num = 5;
 		
-		ArrayList<String> arraylist = new ArrayList<String>();
-		
-		int num = 0;
-		
-		while (num != 6) {
-			
-			System.out.println("Select One Number between 1-6: ");
-			System.out.println("1. Add Pets");
-		    System.out.println("2. Delete Pets");
-		    System.out.println("3. Edit Pets");
-		    System.out.println("4. View Pets");
-		    System.out.println("5. Show a Menu");
-		    System.out.println("6. Exit");
-		    
-
+		while(num != 6) {
+			System.out.println("*** Pet Management System Menu ***");
+			System.out.println("1. Add Pet");
+			System.out.println("2. Delet Pet");
+			System.out.println("3. Edit Pet");
+			System.out.println("4. View Pet");
+			System.out.println("5. Show Pet");
+			System.out.println("6. Exit Pet");
+			System.out.println("Select One Number between 1 - 5: ");
 			num = input.nextInt();
-		    
-		    if(num == 1)
-		    {
-		    	arraylist.addAll(addPet());
-		    }
-		    else if(num == 2)
-		    {
-		    	DeletePet(arraylist);
-		    }
-		    else if(num == 3)
-		    {
-		    	EditPet(arraylist);
-		    }
-		    else if(num == 4)
-		    {
-		    	ViewPet(arraylist);
-		    }
-		    else
-		    {
-		    	continue;
-		    }
-		    
-		    }
-		}
-	
-	public static ArrayList<String> addPet()
-	{
-		try (Scanner input = new Scanner(System.in)) {
-			System.out.println("Add Pet: ");
-			String AddPet = input.next();
-			
-			System.out.println("Pet Name: ");
-			String PetName = input.next();
-			
-			System.out.println("Pet Information: ");
-			String PetInformation = input.next();
-			
-			ArrayList<String> arraylist = new ArrayList<String>();
-			arraylist.add(AddPet);
-			arraylist.add(PetName);
-			arraylist.add(PetInformation);
-			
-			for(String data : arraylist)
-			{
-				System.out.println(data + " ");
+			if(num==1) {
+				addpet();
 			}
-			System.out.println();
-			
-			return arraylist;
-		}
-	}
-	
-	static void DeletePet(ArrayList<String> arraylist)
-	{
-		System.out.println("Delete Pet");
-		
-		arraylist.clear();
-		
-		System.out.println(arraylist);
-		
-	}
-	
-	static void EditPet(ArrayList<String> arraylist)
-	{
-		System.out.println(arraylist);
-		System.out.println("Edit Pet: 1.Add Pet, 2.Pet Name, 3.Pet Information");
-		try (Scanner input = new Scanner(System.in)) {
-			int y = input.nextInt();
-			System.out.println("Edit to: ");
-			try (Scanner input1 = new Scanner(System.in)) {
-				String z = input1.next();
-				arraylist.set(y, z);
+			else if(num==2) {
+				deletepet();
+			}
+			else if(num==3) {
+				editpet();
+			}
+			else if(num==4) {
+				viewpet();
+			}
+			else {
+				continue;
 			}
 		}
-		System.out.println("Edit Pet: " + arraylist);
 	}
 	
-	static void ViewPet(ArrayList<String> arraylist)
-	{
-		System.out.println(arraylist);
+	public static void addpet() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Pet Id:");
+		int PetId = input.nextInt();
+		System.out.print("Pet Name:");
+		String PetName = input.next();
+		System.out.println(PetName);
+		System.out.print("Pet Information:");
+		String PetInformation = input.next();
+		System.out.println(PetInformation);
+		System.out.print("phone number:");
+		String number = input.next();
+		System.out.println(number);
+
 	}
+	
+	public static void deletepet() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Delete Pet");
+	}
+	
+	public static void editpet() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Edit Pet");
+		
+	}
+	
+	public static void viewpet() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("View Pet");
+	}
+	
 	}
 
 
