522b7f1799add21788548f6307758dc031f0e642
bsyseo <bsyseo@naver.com>
2022-05-28 13:56:07 +0900

    added java

---
+++ src/gui/GuiTest.java
@@ -0,0 +1,10 @@
+package gui;
+
+public class GuiTest {
+	public static void main(String[] args) {
+		//MenuSelection meneselection = new MenuSelection();
+	
+		//PetAdder petadder = new PetAdder();
+		PetViewer petviewer = new PetViewer();
+	}
+}
---
+++ src/gui/PetAdder.java
@@ -0,0 +1,50 @@
+package gui;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+
+public class PetAdder extends JFrame{
+	public PetAdder() {
+		JPanel panel = new JPanel(new SpringLayout());
+		panel.setLayout(new SpringLayout());
+		
+		JLabel labelID = new JLabel("ID: ", JLabel.TRAILING);
+		JTextField fieldID = new JTextField(10);
+		labelID.setLabelFor(fieldID);
+		panel.add(labelID);
+		panel.add(fieldID);
+		
+		JLabel labelName = new JLabel("NAME: ", JLabel.TRAILING);
+		JTextField fieldName = new JTextField(10);
+		labelID.setLabelFor(fieldName);
+		panel.add(labelName);
+		panel.add(fieldName);
+		
+		JLabel labelInformation = new JLabel("INFO: ", JLabel.TRAILING);
+		JTextField fieldInformation = new JTextField(10);
+		labelID.setLabelFor(fieldInformation);
+		panel.add(labelInformation);
+		panel.add(fieldInformation);
+		
+		JLabel labelPhone = new JLabel("PHONE: ", JLabel.TRAILING);
+		JTextField fieldPhone = new JTextField(10);
+		labelID.setLabelFor(fieldPhone);
+		panel.add(labelPhone);
+		panel.add(fieldPhone);
+		
+		panel.add(new JButton("save"));
+		panel.add(new JButton("cancle"));
+		
+		SpringUtilities.makeCompactGrid(panel, 5, 2, 6, 6, 6, 6);
+		
+		this.setSize(300,300);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.setContentPane(panel);
+		this.setVisible(true);
+	}
+}
